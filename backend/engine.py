9ec8ae1c71885ce6a08d439f66bb8687f0c6f468
RJSLabbert <riaanjslabbert@gmail.com>
2026-06-03 19:36:49 +0200

    Model_path

--- backend/engine.py
+++ backend/engine.py
@@ -4,7 +4,7 @@ from hailo_platform.genai import LLM
 class ModelEngine:
     def __init__(self):
         self.HailoNPU = VDevice()
-        self.ryuuModel = LLM(vdevice=self.HailoNPU, hef_path="models/Qwen2.5-1.5B-Instruct.hef")
+        self.ryuuModel = LLM(vdevice=self.HailoNPU, model_path="models/Qwen2.5-1.5B-Instruct.hef")
 
     def chatPrompt(self, inputPrompt):
         ModelResponse = self.ryuuModel.generate(inputPrompt)
