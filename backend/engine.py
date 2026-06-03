773e08cb41657a10141d1ffd5abf4cfda1a21aa8
RJSLabbert <riaanjslabbert@gmail.com>
2026-06-03 18:25:42 +0200

    Engine and Main basic

---
+++ backend/engine.py
@@ -0,0 +1,8 @@
+class ModelEngine:
+    def __init__(self):
+        #load qwen model (any model)
+        pass
+
+    def chatPrompt(self, inputPrompt):
+        #HailoRT and NPU call
+        pass    
---
+++ backend/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from engine import ModelEngine
+from pydantic import BaseModel
+
+class TextRequest(BaseModel):
+    request: str
+    
+RyuuLanguageEngine = ModelEngine()
+Ryuu = FastAPI()
+
+@Ryuu.post("/inputText")
+def inputText(request: TextRequest):
+    outputText = RyuuLanguageEngine.chatPrompt(request)
+    return outputText
