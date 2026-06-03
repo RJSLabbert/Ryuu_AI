132ffec41b2ffbd74d696a2ea24aef9b26eafc10
RJSLabbert <riaanjslabbert@gmail.com>
2026-06-03 18:58:36 +0200

    Implemented Hailo and Qwen model

--- backend/engine.py
+++ backend/engine.py
@@ -1,8 +1,11 @@
+from hailo_platform import VDevice
+from hailo_platform.genai import LLM
+
 class ModelEngine:
     def __init__(self):
-        #load qwen model (any model)
-        pass
+        self.HailoNPU = VDevice()
+        self.ryuuModel = LLM(vdevice=self.HailoNPU, hef_path="models/Qwen2.5-1.5B-Instruct.hef")
 
     def chatPrompt(self, inputPrompt):
-        #HailoRT and NPU call
-        pass    
+        ModelResponse = self.ryuuModel.generate(inputPrompt)
+        return ModelResponse      
