930a756cdb6d48eed07e334f986ab01519ff32c9
Riaan Slabbert <70145202+RJSLabbert@users.noreply.github.com>
2026-06-04 08:31:08 +0200

    Refactor chatPrompt to include system prompt
    Updated chatPrompt method to include a system prompt for enhanced context.

--- backend/engine.py
+++ backend/engine.py
@@ -1,15 +1,20 @@
 from hailo_platform import VDevice
 from hailo_platform.genai import LLM
 
+SYSTEM_PROMPT = "You are Ryuu (竜), a private, wise, and Japanese inspired local AI assistant. Answer directly and precisely, with a hint of wisdom and playfulness."
+
 class ModelEngine:
     def __init__(self):
         self.HailoNPU = VDevice()
         self.ryuuModel = LLM(vdevice=self.HailoNPU, model_path="models/Qwen2.5-1.5B-Instruct.hef")
 
     def chatPrompt(self, inputPrompt):
-        userInput = [{"role": "user", "content": inputPrompt}]
+        GenInput = [
+             {"role": "system", "content": SYSTEM_PROMPT},
+             {"role": "user", "content": inputPrompt}
+        ]
         ModelResponse = ""
-        with self.ryuuModel.generate(userInput, max_generated_tokens=500) as tokenGen:
+        with self.ryuuModel.generate(GenInput, max_generated_tokens=500) as tokenGen:
             for token in tokenGen:
                 ModelResponse += token
-        return ModelResponse      
+        return ModelResponse.strip().replace("<|im_end|>","").strip()      
